c410f667ba3a1c0689802e9c5a8bbd491b1ffc25
ariesjunjun <aoarashihazuki@gmail.com>
2025-07-18 11:13:57 +0900

    修正

--- src/app/threads/[threadId]/page.tsx
+++ src/app/threads/[threadId]/page.tsx
@@ -1,5 +1,11 @@
 import ClientThreadContent from "@/components/ClientThreadContent";
 
-export default function ThreadPage({ params }: { params: { threadId: string } }) {
-  return <ClientThreadContent threadId={params.threadId} />;
+type ThreadPageProps = {
+  params: { threadId: string } | Promise<{ threadId: string }>;
+};
+
+export default async function ThreadPage({ params }: ThreadPageProps) {
+  // paramsがPromiseならawait
+  const resolvedParams = await params;
+  return <ClientThreadContent threadId={resolvedParams.threadId} />;
 }
