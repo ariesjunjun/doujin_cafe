9f77edcf9aaa4acd762ffd7d5120953048c4ee66
ariesjunjun <aoarashihazuki@gmail.com>
2025-07-18 11:16:32 +0900

    修正２

--- src/app/threads/[threadId]/page.tsx
+++ src/app/threads/[threadId]/page.tsx
@@ -1,11 +1,10 @@
 import ClientThreadContent from "@/components/ClientThreadContent";
 
 type ThreadPageProps = {
-  params: { threadId: string } | Promise<{ threadId: string }>;
+  params: { threadId: string };
 };
 
 export default async function ThreadPage({ params }: ThreadPageProps) {
-  // paramsがPromiseならawait
-  const resolvedParams = await params;
-  return <ClientThreadContent threadId={resolvedParams.threadId} />;
+  // async関数にするだけでOK
+  return <ClientThreadContent threadId={params.threadId} />;
 }
